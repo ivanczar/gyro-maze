388c8eef391e80aa682501cb03a6b5178449063e
ivanczar <ivanczar2013@gmail.com>
2023-09-28 23:46:53 +1300

    started player class

---
+++ player.py
@@ -0,0 +1,38 @@
+class Player:
+    def __init__(self , starting_pos = (0,0)):
+        self.position = starting_pos
+        self.direction = ""
+        self.speed = 0
+        self.acceleration = 0
+
+    
+    def getSpeed(self):
+        pass
+        
+    def setSpeed(self, newSpeed):
+        pass
+
+    def getAcceleration(self):
+        pass
+
+    def setAcceleration(self, newAcceleration):
+        pass
+
+    def getDirection(self):
+        pass
+
+    def setDirection(self, newDirection):
+        pass
+
+    def getPosition(self):
+        pass
+
+    def setPosition(self, newPosition):
+        pass
+
+    def checkForWalls(self):
+        pass
+
+    def move(self):
+        pass
+
