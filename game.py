814101dcc39cb7fad8bd98403dcdb06c2458372f
Ivan Czar <ivanczar2013@gmail.com>
2023-10-03 17:02:25 +1300

    Added timer

--- game.py
+++ game.py
@@ -2,13 +2,16 @@ from player import Player
 from sense_hat import SenseHat
 from maze import Maze
 from time import sleep
+from timer import Timer
 class Game:
 
     def __init__(self):
         self.s = SenseHat()
         self.maze = Maze()
         self.player = Player(self.maze, self.s)
-
+        self.timer = Timer()
+        
+        self.finishTime = 0
         self.drawLED()
 
 
@@ -18,14 +21,16 @@ class Game:
 
     def drawLED(self):
         print("drawing maze")
+        self.timer.start()
         isPlaying = True
         while isPlaying:
             isPlaying = not self.isWin()
             self.s.set_pixels(self.maze.maze)
             self.s.set_pixel(self.player.position[0], self.player.position[1], (255, 0, 0))
-            sleep(1)
+            sleep(0.5)
             self.player.move()
-        print("YOU WIN!!!")
+        self.finishTime = self.timer.stop()
+        print("YOU WIN in ", self.finishTime)
 
         
 if __name__ == '__main__':
